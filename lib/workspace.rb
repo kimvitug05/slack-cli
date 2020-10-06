0d0c963af0eff2d622135ccbe38f76b39fa736ed
Kimberly Vitug <kim_vitug05@yahoo.com>
2020-10-05 22:24:51 -0700

    add select_channel method in Workspace

--- lib/workspace.rb
+++ lib/workspace.rb
@@ -8,11 +8,21 @@ class Workspace
 
   def select_user(name = nil, id = nil)
     if name
-      @user.find { |user| user[:name] == name }
+      @users.find { |user| user[:name] == name }
     elsif id
-      @user.find { |user| user[:id] == id }
+      @users.find { |user| user[:id] == id }
     else
-      raise ArgumentError, "Please enter a valid name or id."
+      raise ArgumentError, "Please enter a valid user name or id."
+    end
+  end
+
+  def select_channel(channel_name = nil, id = nil)
+    if channel_name
+      @channels.find { |channel| channel[:name] == channel_name }
+    elsif id
+      @channels.find { |channel| channel[:id] == id }
+    else
+      raise ArgumentError, "Please enter a valid channel name or id."
     end
   end
 end
