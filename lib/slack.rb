3fc3181f59eea4376e3eb6155607bfc2d0078607
Kimberly Vitug <kim_vitug05@yahoo.com>
2020-10-05 19:38:17 -0700

    add list_channel method

--- lib/slack.rb
+++ lib/slack.rb
@@ -6,17 +6,28 @@ CHANNELS_URL = "https://slack.com/api/conversations.list"
 
 Dotenv.load
 
+def list_channels
+  response = HTTParty.get(CHANNELS_URL, query: {
+      token: "xoxp-1222171918129-1261274944689-1408480907106-983ae5e232993a08337168b03cc11e74",
+  })
+
+  return response["channels"].map do |channel|
+    {
+        name: channel["name"],
+        topic: channel["topic"]["value"],
+        member_count: channel["num_members"],
+        id: channel["id"]
+    }
+  end
+end
+
 def main
   puts "Welcome to the Ada Slack CLI!"
   workspace = Workspace.new
 
   # TODO project
 
-  response = HTTParty.get(CHANNELS_URL, query: {
-      token: ENV["slack_token"],
-  })
-
-  pp response
+  puts list_channels
 
   puts "Thank you for using the Ada Slack CLI"
 end
